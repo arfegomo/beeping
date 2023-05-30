e61c6fe9fa7d296d0e3325a4f30d4ac7a05858c0
arfegomo <ariel.fernando.gonzalez.moreno@gmail.com>
2023-05-30 19:09:22 +0200

    JOB Async Costo Total Ordenes - Command

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -8,4 +8,18 @@ use Illuminate\Database\Eloquent\Model;
 class Order extends Model
 {
     use HasFactory;
+    
+    public function productos(){
+            
+        return $this->belongsToMany('\App\Models\Product', 'orders_lines')->withPivot('qty');
+        
+    }
+
+    public function costeSum(){
+        
+        return $this->belongsToMany('\App\Models\Product', 'orders_lines')
+        ->selectRaw('sum(orders_lines.qty * products.cost) as pivot_count')
+        //->withTimestamps()
+        ->groupBy('pivot_product_id','pivot_order_id');
+    }
 }
